acd52af74a14f8f0919adb5d14a7497fa636ac89
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-07-04 13:08:16 +0200

    FIX: Intialization

--- src/main_light.py
+++ src/main_light.py
@@ -69,7 +69,12 @@ def main(args):
                       validation_samples=len(qm9_datamodule.val_dataloader().dataset),
                       test_samples=len(qm9_datamodule.test_dataloader().dataset),
                        mae=mad, mad=mad, mean=mean, lr=args.lr, weight_decay=args.weight_decay)
-    trainer = L.Trainer(check_val_every_n_epoch=20, deterministic=True, max_epochs=args.epochs, gradient_clip_val=args.gradient_clip, enable_checkpointing=False, accelerator=args.device, devices=1, logger=wandb_logger)# accelerator='gpu', devices=1)
+    trainer = L.Trainer(
+                        check_val_every_n_epoch=20, deterministic=True, max_epochs=args.epochs,
+                        gradient_clip_val=args.gradient_clip, enable_checkpointing=False,
+                        accelerator=args.device, devices=1, logger=wandb_logger)# accelerator='gpu', devices=1)
+
+
     tuner = L.pytorch.tuner.Tuner(trainer)
     tuner.scale_batch_size(empsn, mode='binsearch', datamodule=qm9_datamodule)
 
