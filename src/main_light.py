205fd8e262f3b4ca89ddaed14999f9d8cbdddbd7
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-07-17 17:33:51 +0100

    FIX: IMportrs

--- src/main_light.py
+++ src/main_light.py
@@ -75,8 +75,8 @@ def main(args):
     if len(models):
         best_model = os.path.join(ckpt_folder, best_model) 
 
-    best_checkpoint = L.callbacks.ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=1, dir_path='models', filename='empsn-{epoch}-{val_loss:.2f}')
-    latest_checkpoint = L.callbacks.ModelCheckpoint(monitor='epoch', mode='max', save_top_k=1, filename='latest-{epoch}-{step}', every_n_epochs=20)
+    best_checkpoint = L.pytorch.callbacks.ModelCheckpoint(monitor='val_loss', mode='min', save_top_k=1, dir_path='models', filename='empsn-{epoch}-{val_loss:.2f}')
+    latest_checkpoint = L.pytorch.callbacks.ModelCheckpoint(monitor='epoch', mode='max', save_top_k=1, filename='latest-{epoch}-{step}', every_n_epochs=20)
 
     empsn = LitEMPSN(model, train_samples=len(qm9_datamodule.train_dataloader().dataset),
                       validation_samples=len(qm9_datamodule.val_dataloader().dataset),
